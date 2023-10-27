9784e40302f19a9d9223e71891887da53e6be263
pi <pi@pi.com>
2023-10-27 11:43:51 -0500

    big refactor so the code makes sense

--- src/tg-bot/bot.ts
+++ src/tg-bot/bot.ts
@@ -8,28 +8,21 @@ import { startDecryption } from "./startDecryption";
 import { uploadDecryptedFile } from "./uploadDecryptedFile";
 import { Api } from "telegram";
 
-let isBusy = false;
+let isProcessing = false;
 const requestQueue: { trackId: string; message: Api.Message }[] = [];
-const admins = process.env.ADMIN_IDS?.split(",") || [];
+const adminIds = process.env.ADMIN_IDS?.split(",") || [];
 
-async function finished(mongoDB?: MongoDB) {
+async function cleanUp(mongoDB?: MongoDB) {
   console.log("Cleaning up...");
+  isProcessing = false;
+  if (mongoDB) await mongoDB.close();
+  processNextRequest();
+}
 
-  // Reset states, get ready for next app
-  isBusy = false;
-
-  if (mongoDB) {
-    // Close the MongoDB connection
-    await mongoDB.close();
-  }
-
-  // Process next request in the queue
-  if (requestQueue.length > 0) {
-    const nextRequest = requestQueue.shift();
-    if (nextRequest) {
-      await handleRequest(nextRequest.message);
-    }
-  }
+async function processNextRequest() {
+  if (requestQueue.length === 0) return;
+  const nextRequest = requestQueue.shift();
+  if (nextRequest) await handleAppRequest(nextRequest.message);
 }
 
 async function handleReboot(message: Api.Message) {
@@ -43,52 +36,48 @@ async function handleReboot(message: Api.Message) {
     `${dirname(__dirname)}/ipa-files/encrypted/*`,
   );
 
-  const sendTo = message.chatId?.toString() as string;
-  await client.sendMessage(sendTo, {
+  const sendToChatId = message.chatId?.toString() as string;
+  await client.sendMessage(sendToChatId, {
     message: "Done",
     replyTo: message,
   });
 }
 
 export async function handleDelete(message: Api.Message) {
-  const sendTo = message.chatId?.toString() as string;
-  const urlParts = message.text.trim().split(" ");
-  if (urlParts.length < 2 || !message.text.includes("apps.apple.com")) {
-    await client.sendMessage(sendTo, {
+  const sendToChatId = message.chatId?.toString() as string;
+  const parsedUrl = parseAppStoreUrl(message.text);
+
+  if (!parsedUrl.isValid) {
+    await client.sendMessage(sendToChatId, {
       message: "❌ Invalid App Store URL.",
       replyTo: message,
     });
-    return 1;
+    await cleanUp();
+    return;
   }
 
-  const url = urlParts[1].trim();
-  const idRegex = /\/id(\d+)/;
-  const match = url.match(idRegex);
-  const trackId = match ? match[1] : null;
+  const { trackId } = parsedUrl;
 
   if (trackId && trackId.length && trackId.length < 15) {
     const mongoDB = MongoDB.getInstance();
     await mongoDB.connect();
-
-    // Access a collection
     const collection = mongoDB.getCollection("app_info_collection");
 
-    // Should never happen
     if (!collection) {
-      console.log("It happened??");
-      await finished(mongoDB);
-      return 1;
+      console.error("MongoDB collection not found");
+      await cleanUp(mongoDB);
+      return;
     }
 
     // Lookup the last document by trackId and delete it
     let appInfo = await collection.findOneAndDelete({ trackId });
     if (appInfo.ok) {
-      await client.sendMessage(sendTo, {
+      await client.sendMessage(sendToChatId, {
         message: `✅ Deleted app with trackId: ${trackId}`,
         replyTo: message,
       });
     } else {
-      await client.sendMessage(sendTo, {
+      await client.sendMessage(sendToChatId, {
         message: `❌ Failed to delete app with trackId: ${trackId}`,
         replyTo: message,
       });
@@ -96,22 +85,13 @@ export async function handleDelete(message: Api.Message) {
   }
 }
 
-async function handleRequest(message: Api.Message) {
-  const sendTo = message.chatId?.toString() as string;
-  const urlParts = message.text.trim().split(" ");
-  if (
-    urlParts.length < 2 ||
-    !/apps\.apple\.com|itunes\.apple\.com/.test(message.text)
-  ) {
-    await client.sendMessage(sendTo, {
-      message: "❌ Invalid App Store URL.",
-      replyTo: message,
-    });
-    await finished();
-    return 1;
+function parseAppStoreUrl(url: string) {
+  const urlParts = url.trim().split(" ");
+  if (urlParts.length < 2 || !/apps\.apple\.com|itunes\.apple\.com/.test(url)) {
+    return { isValid: false };
   }
 
-  const url = urlParts[1].trim();
+  url = urlParts[1].trim();
 
   const idRegex = /\/id(\d+)/;
   const countryRegex = /apple\.com\/(\w\w)\//;
@@ -123,142 +103,205 @@ async function handleRequest(message: Api.Message) {
   const countryCode = matchCountry ? matchCountry[1] : "us";
 
   if (!(trackId && trackId.length && trackId.length < 15)) {
-    await client.sendMessage(sendTo, {
-      message: "❌ Invalid App Store URL.",
-      replyTo: message,
-    });
-    await finished();
-    return 1;
+    return { isValid: false };
   }
 
-  if (isBusy) {
-    if (requestQueue.find((request) => request.trackId === trackId)) {
-      await client.sendMessage(sendTo, {
-        message: `⌛ That app is already in the queue. Please wait...`,
-        replyTo: message,
-      });
-      return;
-    }
+  return { isValid: true, trackId, countryCode };
+}
 
-    // Add the request to the queue if maximum limit not reached
-    if (requestQueue.length >= 5) {
-      await client.sendMessage(sendTo, {
-        message: `⌛ Maximum request limit of 5 reached. Please try again later.`,
-        replyTo: message,
-      });
-      return;
-    }
-
-    await client.sendMessage(sendTo, {
-      message: `⏳ Your request has been added to the queue. Please wait...`,
+async function handleQueue(
+  message: Api.Message,
+  sendToChatId: string,
+  trackId: string,
+) {
+  if (requestQueue.find((request) => request.trackId === trackId)) {
+    await client.sendMessage(sendToChatId, {
+      message: `⌛ That app is already in the queue. Please wait...`,
       replyTo: message,
     });
-
-    requestQueue.push({ trackId, message });
     return;
   }
 
-  isBusy = true;
-
-  const mongoDB = MongoDB.getInstance();
-  await mongoDB.connect();
-
-  // Access a collection
-  const collection = mongoDB.getCollection("app_info_collection");
-
-  // Should never happen
-  if (!collection) {
-    console.log("It happened??");
-    await finished(mongoDB);
-    return 1;
+  // Add the request to the queue if maximum limit not reached
+  if (requestQueue.length >= 5) {
+    await client.sendMessage(sendToChatId, {
+      message: `⌛ Maximum request limit of 5 reached. Please try again later.`,
+      replyTo: message,
+    });
+    return;
   }
 
-  // Lookup the last document by trackId
-  let appInfo = await collection.findOne<MongoApp>(
-    { trackId },
-    { sort: { _id: -1 } },
-  );
+  await client.sendMessage(sendToChatId, {
+    message: `⏳ Your request has been added to the queue. Please wait...`,
+    replyTo: message,
+  });
+
+  requestQueue.push({ trackId: trackId!, message });
+  return;
+}
+
+async function findAppInfo(trackId: string) {
+  const mongoDB = MongoDB.getInstance();
+  await mongoDB.connect();
+  const collection = mongoDB.getCollection("app_info_collection");
+
+  if (!collection) {
+    console.error("MongoDB collection not found");
+    await cleanUp(mongoDB);
+    return;
+  }
+
+  return await collection.findOne<MongoApp>({ trackId }, { sort: { _id: -1 } });
+}
+
+async function decryptAndDownloadApp(
+  message: Api.Message,
+  trackId: string,
+  countryCode: string,
+  sendTo: string,
+) {
+  console.log("Downloading app for trackId:", trackId);
+  await startDecryption({
+    message,
+    trackId,
+    countryCode,
+    sendTo,
+  });
+
+  const appInfo = await findAppInfo(trackId);
+  if (!appInfo) throw new Error("App not found after decryption");
+
+  await uploadDecryptedFile(appInfo, message);
+  return appInfo;
+}
+
+async function sendExistingFileMessage(
+  appInfo: MongoApp,
+  message: Api.Message,
+) {
+  const msgs = await client.getMessages(appInfo.chatId, {
+    ids: [appInfo.fileId],
+  });
+
+  if (Array.isArray(msgs) && msgs.length && message.chat) {
+    await client.sendMessage(message.chat, {
+      message: msgs[0],
+      replyTo: message,
+    });
+  } else {
+    console.error("No message found for trackId:", appInfo.trackId);
+  }
+}
+
+async function handleExistingAppInfo(appInfo: MongoApp, message: Api.Message) {
+  if (!parseInt(`${appInfo.fileId || "0"}`, 10)) {
+    console.log("File not found for trackId:", appInfo.trackId);
+    await uploadDecryptedFile(appInfo, message);
+  } else {
+    console.log("File found for trackId:", appInfo.trackId);
+    await sendExistingFileMessage(appInfo, message);
+  }
+}
+
+async function handleAppRequest(message: Api.Message) {
+  const sendToChatId = message.chatId?.toString() as string;
+  const parsedUrl = parseAppStoreUrl(message.text);
+
+  if (!parsedUrl.isValid) {
+    await client.sendMessage(sendToChatId, {
+      message: "❌ Invalid App Store URL.",
+      replyTo: message,
+    });
+    await cleanUp();
+    return;
+  }
+
+  const { trackId, countryCode } = parsedUrl;
+
+  if (isProcessing) {
+    await handleQueue(message, sendToChatId, trackId!);
+    return;
+  }
+
+  isProcessing = true;
+
+  let appInfo = await findAppInfo(trackId!);
 
   try {
     if (!appInfo) {
-      console.log("Downloading app for trackId:", trackId);
-      await startDecryption({ message, trackId, countryCode, sendTo });
-
-      // Lookup the app we just decrypted by trackId
-      appInfo = await collection.findOne<MongoApp>(
-        { trackId },
-        { sort: { _id: -1 } },
+      appInfo = await decryptAndDownloadApp(
+        message,
+        trackId!,
+        countryCode!,
+        sendToChatId,
       );
-
-      // TODO: Safe to ignore?
-      if (!appInfo) {
-        console.log("Not safe to ignore lol");
-        // Should never reach here but decided to add this just in case
-        console.error("No app found still for trackId:", trackId);
-        await finished(mongoDB);
-        return 1;
-      }
-
-      await uploadDecryptedFile(appInfo, message);
-    } else if (!parseInt(`${appInfo.fileId || "0"}`, 10)) {
-      console.log("No file found for trackId:", trackId);
-      await uploadDecryptedFile(appInfo, message);
     } else {
-      console.log("File found for trackId:", trackId);
-      const msgs = await client.getMessages(appInfo.chatId, {
-        ids: [appInfo.fileId],
-      });
-
-      if (Array.isArray(msgs) && msgs.length && message.chat) {
-        await client.sendMessage(message.chat, {
-          message: msgs[0],
-          replyTo: message,
-        });
-      } else {
-        console.error("No message found for trackId:", trackId);
-      }
+      await handleExistingAppInfo(appInfo, message);
     }
   } catch (error) {
     console.error("Error occurred:", error);
-  } finally {
-    if (appInfo) {
-      // Just double make sure its all removed, in the event there's errors
-      const decrypted = `${dirname(__dirname)}/ipa-files/decrypted/${
-        appInfo.filename
-      }`;
-      await deleteSingleFile(decrypted);
+  }
 
-      // Could not be downloaded
-      // This one is the bundle id with a wildcard, since we don't know the exact filename
-      const encrypted = `${dirname(__dirname)}/ipa-files/encrypted/${
-        appInfo.bundleId
-      }*`;
-      await deleteFilesMatchingPattern(encrypted);
-    }
+  if (appInfo) {
+    // Just double make sure files got cleaned up, in the event there's errors
+    const decrypted = `${dirname(__dirname)}/ipa-files/decrypted/${
+      appInfo.filename
+    }`;
+    await deleteSingleFile(decrypted);
 
-    await finished(mongoDB);
+    // Could not be downloaded
+    // This one is the bundle id with a wildcard, since we don't know the exact filename
+    const encrypted = `${dirname(__dirname)}/ipa-files/encrypted/${
+      appInfo.bundleId
+    }*`;
+    await deleteFilesMatchingPattern(encrypted);
+  }
+
+  await cleanUp();
+}
+
+function isAuthorizedPrivateChat(event: NewMessageEvent) {
+  return event.isPrivate && adminIds.includes(`${event.message.senderId}`);
+}
+
+function isAuthorizedGroupChat(event: NewMessageEvent) {
+  const sendToChatId = event.message.chatId?.toString() as string;
+  const isGroupChatAllowed =
+    !event.isPrivate && CHAT_IDS.includes(sendToChatId);
+
+  return isGroupChatAllowed && isAuthorizedGroupTopic(event);
+}
+
+function isAuthorizedAdmin(event: NewMessageEvent) {
+  return event.chatId && adminIds.includes(`${event.message.senderId}`);
+}
+
+function isAuthorizedGroupTopic(event: NewMessageEvent) {
+  const message = event.message;
+  const topicId = message.replyToMsgId;
+  const sendToChatId = message.chatId?.toString() as string;
+  const index = CHAT_IDS.indexOf(sendToChatId);
+  const allowedTopicId = parseInt(TOPIC_IDS[index], 10);
+
+  if (topicId === undefined && allowedTopicId === 0) return true; // not a topic group
+  return allowedTopicId === topicId;
+}
+
+async function handleRegularCommands(message: Api.Message) {
+  if (message.text.startsWith("/request")) handleAppRequest(message);
+}
+
+async function handleAdminCommands(event: NewMessageEvent) {
+  if (isAuthorizedAdmin(event)) {
+    const { message } = event;
+    if (message.text.startsWith("/reboot")) await handleReboot(message);
+    if (message.text.startsWith("/delete")) await handleDelete(message);
   }
 }
 
 client.addEventHandler(async (event) => {
-  const message = event.message;
-  const sendTo = message.chatId?.toString() as string;
-
-  if (event.isPrivate && !admins.includes(`${event.message.senderId}`)) return;
-  if (!event.isPrivate && !CHAT_IDS.includes(sendTo)) return 1;
-
-  // Lock down to certain topicId's
-  const topicId = message.replyToMsgId;
-  const index = CHAT_IDS.indexOf(sendTo);
-  if (parseInt(TOPIC_IDS[index], 10) && TOPIC_IDS[index] !== `${topicId}`) {
-    console.error("Unauthorized topicId");
-    return 1;
+  if (isAuthorizedPrivateChat(event) || isAuthorizedGroupChat(event)) {
+    await handleRegularCommands(event.message);
+    await handleAdminCommands(event);
   }
-
-  if (message.text.startsWith("/request")) handleRequest(message);
-
-  // Admin commands
-  if (!event.chatId || !admins.includes(`${message.senderId}`)) return;
-  if (message.text.startsWith("/reboot")) handleReboot(message);
-  if (message.text.startsWith("/delete")) handleDelete(message);
 }, new NewMessage());
